de25734b01da79ec067cbe36a3ba26a8e363d7a5
HADIRIDA4 <hadirida444@gmail.com>
2023-08-07 16:41:12 +0300

    test_1

--- char_frequency_Hadi_Rida.py
+++ char_frequency_Hadi_Rida.py
@@ -4,4 +4,4 @@ def char_frequency (s) :
         Letter_count[letter]=Letter_count.get(letter, 0)+1
     print(Letter_count)
 text=input("please enter your text ")
-char_frequency(text) 
+char_frequency(text)
